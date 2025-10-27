4604879d9978bd1ea2527fcfe8321a26d29c6d41
Nguyen Nhat Quan <NguyenNhatQuan06022004@gmail.com>
2025-10-27 14:07:09 +0700

    initial project

---
+++ TinyFarmProject/Assets/Scripts/demo.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class demo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Console.WriteLine("HELLO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
